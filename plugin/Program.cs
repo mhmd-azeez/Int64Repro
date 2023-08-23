e75b0ab21a971c0f3703529016a426afb11fb14e
Muhammad Azeez <muhammad-azeez@outlook.com>
2023-08-24 02:00:06 +0300

    checkpoint 1

--- plugin/Program.cs
+++ plugin/Program.cs
@@ -1,10 +1,11 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Runtime.InteropServices;
 
 public class Program
 {
-    [MethodImpl(MethodImplOptions.InternalCall)]
+    //[MethodImpl(MethodImplOptions.InternalCall)]
     // This doesn't work
-    public static unsafe extern long do_something(long n);
+    //[DllImport("__internal")]
+    //public static unsafe extern long do_something(long n);
 
     // This doesn't work
     // public static unsafe extern long do_something(int n, int n2);
@@ -18,9 +19,15 @@ public class Program
     // This doesn't work
     // public static unsafe extern int do_something(int n, int n2, int n3, int n4, int n5);
 
+    // [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+
+    [DllImport("mylib")]
+    public static extern void something();
+
     public static void Main(string[] args)
     {
-        var result = do_something(5);
-        Console.WriteLine($"Hello World! {result}");
+        //var result = do_something(5);
+        something();
+        Console.WriteLine($"Hello World!");
     }
 }
--- plugin/native.c
+++ plugin/native.c
@@ -1,35 +1,72 @@
 #pragma once
-#define NDEBUG
-// https://github.com/dotnet/runtime/blob/v7.0.0/src/mono/wasi/mono-wasi-driver/driver.c
 #include <string.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
 
 #include <mono-wasi/driver.h>
 #include <mono/metadata/exception.h>
 
-#include <stdint.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 #define IMPORT(a, b) __attribute__((import_module(a), import_name(b)))
 
-// This doesn't work
-IMPORT("env", "do_something") extern int64_t do_something(int64_t);
+typedef struct {
+    const char* name;
+    void* addr;
+} MonoDlMapping;
 
-// This works
-// IMPORT("env", "do_something") extern int64_t do_something(int32_t);
+int count = 1;
+void do_something(void) {
+    int x = 1;
+    x++;
+    count += x;
+    // Your function implementation
+}
 
-// This doesn't work
-// IMPORT("env", "do_something") extern int64_t do_something(int32_t, int32_t);
+static MonoDlMapping hardcoded_mapping = { "do_something", &do_something };
 
-// This works
-// IMPORT("env", "do_something") extern int32_t do_something(int32_t, int32_t);
+char* basic_strdup(const char* source) {
+    if (!source) return NULL;
+    char* dest = (char*)malloc(strlen(source) + 1);
+    if (dest) {
+        strcpy(dest, source);
+    }
+    return dest;
+}
 
-// This doesn't work
-// IMPORT("env", "do_something") extern int32_t do_something(int32_t, int32_t, int32_t, int32_t, int32_t);
+static void*
+dl_mapping_open(const char* file, int flags, char** err, void* user_data) {
+   
+    if (strcmp(file, "mylib") == 0) {
+        *err = basic_strdup("");
+        return &hardcoded_mapping;
+    }
+    else {
+        *err = basic_strdup("File not registered");
+        return NULL;
+    }
+}
+
+static void*
+dl_mapping_symbol(void* handle, const char* symbol, char** err, void* user_data) {
+    return hardcoded_mapping.addr;
+    if (strcmp(symbol, hardcoded_mapping.name) == 0) {
+        *err = basic_strdup("");
+        return hardcoded_mapping.addr;
+    }
+    else {
+        *err = basic_strdup("Symbol not found");
+        return NULL;
+    }
+}
 
 
-void repro_attach_internal_calls()
-{
-    mono_add_internal_call("Program::do_something", do_something);
+
+void mono_dl_register_library() {
+    mono_dl_fallback_register(dl_mapping_open, dl_mapping_symbol, NULL, NULL);
+    //hardcoded_mapping.addr = do_something;
+}
+
+void repro_attach_internal_calls() {
+    mono_dl_register_library();
+    // mono_add_internal_call("Program::do_something", do_something_wrapper); // Assuming this exists somewhere
 }
