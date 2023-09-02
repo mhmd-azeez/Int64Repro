6ef83e4de3a39745fcb4106de3d6e77258ee74c2
Muhammad Azeez <muhammad-azeez@outlook.com>
2023-09-02 12:46:38 +0300

    Revert "checkpoint 1"
    This reverts commit e75b0ab21a971c0f3703529016a426afb11fb14e.

--- plugin/Program.cs
+++ plugin/Program.cs
@@ -1,11 +1,10 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
 public class Program
 {
-    //[MethodImpl(MethodImplOptions.InternalCall)]
+    [MethodImpl(MethodImplOptions.InternalCall)]
     // This doesn't work
-    //[DllImport("__internal")]
-    //public static unsafe extern long do_something(long n);
+    public static unsafe extern long do_something(long n);
 
     // This doesn't work
     // public static unsafe extern long do_something(int n, int n2);
@@ -19,15 +18,9 @@ public class Program
     // This doesn't work
     // public static unsafe extern int do_something(int n, int n2, int n3, int n4, int n5);
 
-    // [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
-
-    [DllImport("mylib")]
-    public static extern void something();
-
     public static void Main(string[] args)
     {
-        //var result = do_something(5);
-        something();
-        Console.WriteLine($"Hello World!");
+        var result = do_something(5);
+        Console.WriteLine($"Hello World! {result}");
     }
 }
